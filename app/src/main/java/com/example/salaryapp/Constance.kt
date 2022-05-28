027b4b7edaf238e159f5a7888825861bc6a01b4f
seregious <seregious@gmail.com>
2022-05-29 01:14:03 +0300

    setup logic

---
+++ app/src/main/java/com/example/salaryapp/Constance.kt
@@ -0,0 +1,9 @@
+package com.example.salaryapp
+
+object Constance {
+    const val directorSalary = 50000
+    const val someoneSalary = 50
+
+    const val director = "sergey"
+    const val someone = "someone"
+}
--- app/src/main/java/com/example/salaryapp/MainActivity.kt
+++ app/src/main/java/com/example/salaryapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.salaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.salaryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView2.visibility = View.VISIBLE
+            var enterText = binding.editTextTextPersonName.text.toString()
+
+            when(enterText) {
+                Constance.director ->  {
+                    val sergeyText = "your salary is ${Constance.directorSalary}"
+                    binding.textView2.text = sergeyText
+                }
+
+                Constance.someone -> {
+                    val someoneText = "your salary is ${Constance.someoneSalary}"
+                    binding.textView2.text = someoneText
+                }
+
+                else -> {
+
+                    binding.textView2.text = "hello"
+                }
+            }
+        }
     }
 }
